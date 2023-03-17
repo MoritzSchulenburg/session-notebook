e7ecec18d9ceedbd67bb9604914e9851d54fcbb1
Moritz Schulenburg <moritzschulenburg@web.de>
2023-03-17 11:16:23 +0100

    save next steps

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,55 @@
-// js file
-// this is the test for the second run
+var oldUse = 'Tryy not to use';
+
+let newVariableChange = 'I can change yyou'
+
+const myValueWillNotChange = 'you are stuck with me'
+
+console.log('old use', oldUse);
+
+console.log('newVariableChange:', newVariableChange);
+
+console.log('myValueWillNotChange: ' myValueWillNotChange);
+
+newVariableChange= 10;
+console.log('new value of newVariableChange' , newVariableChange);
+
+let myboolean = false;
+
+let myString = 'This is a text? 129';
+console.log();
+
+let myNumber = 10;
+
+let myBigNumber = 3000n;
+
+let myNull = null;
+
+let myArray = ['Element 1', 2, 'Element 3', false, 5];
+console.log('');
+
+let myObject = {
+
+cohortName: "Savory",
+amountStudents:14,
+academy:"Spiced";
+knowJS: true;
+}
+
+let myUndefine;
+console.log('Myy Undefine: ', myUndefine);
+
+console.log('-------------------');
+
+const isANumber = '1';
+const amIANumber = 1;
+
+console.log('isANumber: ', isANumber);
+console.log('amIANumber: ', amIANumber);
+
+let suma = parseInt(isANumber) + amIANumber;
+console.log('Suma: ', suma);
+
+let math = amIANumber - isANumber;
+console.log('math 1: ', math);
+
+console.log();
